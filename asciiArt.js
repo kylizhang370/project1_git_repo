f2f8512474f130f6227b4e9b01bb7fbf61890354
Zhanhong Liang <zhanhongliang@Zhanhongs-MacBook-Pro.local>
2023-04-17 23:04:32 -0700

    Third Commit make the asciiArt.js to use process.argv

---
+++ asciiArt.js
@@ -0,0 +1,7 @@
+if (process.argv.length > 2) {
+    const inputString = process.argv[2];
+    console.log("Input string:", inputString);
+  } else {
+    console.log("Give an input string.");
+  }
+  
