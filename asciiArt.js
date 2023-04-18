6c76a15525c631ca401c95f7d1b3a2faf17cdba7
Zhanhong Liang <zhanhongliang@Zhanhongs-MacBook-Pro.local>
2023-04-17 23:30:16 -0700

    Fourth commit to generate ASCII art using Figlet

--- asciiArt.js
+++ asciiArt.js
@@ -1,7 +1,36 @@
+const figlet = require('figlet');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// in the case of command line arugment
 if (process.argv.length > 2) {
     const inputString = process.argv[2];
-    console.log("Input string:", inputString);
-  } else {
-    console.log("Give an input string.");
-  }
-  
+    figlet(inputString, (error, inputData) => {
+      if (error) {
+        console.log('Error:', error);
+      } else {
+        console.log(inputData);
+      }
+
+      rl.close();
+      process.exit();
+    });
+  // if there is no command line 
+} else {
+  rl.question('Enter an input string to turn into ASCII art: ', (inputString) => {
+    figlet(inputString, (error, inputData) => {
+      if (error) {
+        console.log('Error:', error);
+      } else {
+        console.log(inputData);
+      }
+    });
+
+    // Close the readline here 
+    rl.close();
+  });
+}
